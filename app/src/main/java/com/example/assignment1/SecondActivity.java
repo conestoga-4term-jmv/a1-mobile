4e584e50a6e936ab9bbe1158552b5daea8dd704f
Joao Eduardo Beraldo Iada <joaoed.beraldo@gmail.com>
2025-01-29 17:05:50 -0500

    Add first Page
    Completed first Page

---
+++ app/src/main/java/com/example/assignment1/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity{
+
+}
